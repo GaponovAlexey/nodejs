ef00904418c8d4c14b29909a74f11f309e503ea0
kekelberi <alexei.gaponov@gmail.com>
2022-12-10 16:27:29 -0600

    save

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const {gameOptions, RestartGame} = require('./options')
 
 const TelegramApi = require("node-telegram-bot-api");
 const token = process.env.ENV_TOKEN;
@@ -6,34 +7,7 @@ const token = process.env.ENV_TOKEN;
 var opt = { polling: true };
 const bot = new TelegramApi(token, opt);
 
-const gameOptions = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: "1", callback_data: "1" },
-        { text: "2", callback_data: "2" },
-        { text: "3", callback_data: "3" },
-      ],
-      [
-        { text: "4", callback_data: "4" },
-        { text: "5", callback_data: "5" },
-        { text: "6", callback_data: "6" },
-      ],
-      [
-        { text: "7", callback_data: "7" },
-        { text: "8", callback_data: "8" },
-        { text: "9", callback_data: "9" },
-      ],
-      [{ text: "0", callback_data: "0" }],
-    ],
-  }),
-};
 
-const RestartGame = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: "Restart", callback_data: "/again" }]],
-  }),
-};
 
 //start bot
 const start = async () => {
